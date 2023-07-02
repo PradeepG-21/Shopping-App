ca01423bb49f68752dae0eea8c27c2f978cbf48f
PradeepG-21 <pradeep21.ganesh@gmail.com>
2023-07-02 11:38:35 +0530

    App Initialization

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import {createRoot} from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+const el = document.getElementById('root')
+const root = createRoot(el)
+
+root.render(
+    <div>
+        <App />
+    </div>
+)
